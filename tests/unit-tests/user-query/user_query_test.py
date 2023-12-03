05334cc4e6b6d1f977a13ff2023186745ec61b78
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-12-03 19:13:33 +0100

    WIP

--- tests/unit-tests/user-query/user_query_test.py
+++ tests/unit-tests/user-query/user_query_test.py
@@ -1,10 +1,14 @@
+import sys
+
 from user_query import handler
+
+sys.path.append('infra/lambda/user-query/python3')
 
 S_QUERY = 'stroke AND single cell rna seq AND musculus'
 
 
 def test_user_query():
     request_body_as_received_by_lambda = {'body': '{\n  "ncbi_query": "' + S_QUERY + '"\n}'}
-    expected_lambda_response = {'statusCode': 200}
+    expected_lambda_response = {'statusCode': 200, 'headers': {'content-type': 'application/json'}, 'body': ''}
 
     assert expected_lambda_response == handler(event=request_body_as_received_by_lambda, context=None)
