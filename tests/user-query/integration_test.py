5b5f69b70507ed0ebeb060576a5cad9d2bd40c55
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-12-04 21:08:12 +0100

    Fix tests

--- tests/user-query/integration_test.py
+++ tests/user-query/integration_test.py
@@ -1,3 +1,5 @@
+import json
+
 import urllib3
 
 ENDPOINT = 'https://hxtfx8byeb.execute-api.eu-central-1.amazonaws.com/user_query_lambda_stage/query-study-hierarchy'
@@ -5,7 +7,8 @@ http = urllib3.PoolManager()
 
 S_QUERY = 'stroke AND single cell rna seq AND musculus'
 
+
 def test_xs_request():
     request_body = {'ncbi_query': S_QUERY}
-    response = http.request('GET', ENDPOINT, request_body)
-    assert {'statusCode': 200} == response.data
+    response = http.request('GET', ENDPOINT, json=request_body)
+    assert 200 == response.status
