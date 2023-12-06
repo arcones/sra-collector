a62763396167028fb37f1e67c4731c29f17f9f57
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-12-06 16:52:17 +0100

    WIP

---
+++ utils/purge-queues.py
@@ -0,0 +1,18 @@
+import logging
+
+import boto3
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(filename)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger('user_query')
+logger.setLevel(logging.INFO)
+
+sqs = boto3.client('sqs', region_name='eu-central-1')
+
+logger.info(f'Listing queues...')
+queue_urls = sqs.list_queues()['QueueUrls']
+logger.info(f'Listed {len(queue_urls)} queues')
+
+for queue_url in queue_urls:
+    logger.info(f'Purging queue {queue_url}...')
+    response = sqs.purge_queue(QueueUrl=queue_url)
+    logger.info(f'Purged queue {queue_url}')
