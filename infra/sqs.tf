180cfac5ac4e67c01befa8fcb73b2a5fa0b7b3a4
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-12-06 19:01:53 +0100

    WIP

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -2,19 +2,16 @@ resource "aws_sqs_queue" "study_ids_queue" {
   name                        = "study_ids_queue.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-  deduplication_scope         = "messageGroup"
 }
 
 resource "aws_sqs_queue" "pending_srp_queue" {
   name                        = "pending_srp_queue.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-  deduplication_scope         = "messageGroup"
 }
 
 resource "aws_sqs_queue" "study_summaries_queue" {
   name                        = "study_summaries_queue.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
-  deduplication_scope         = "messageGroup"
 }
