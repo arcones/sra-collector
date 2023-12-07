d789e418c18da9ec1643126a15e510c4ae67b073
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-12-07 19:05:47 +0100

    :speech_balloon: Add lambda for processing studies with missing SRPs

---
+++ infra/lambdas/missing_srps/iam.tf
@@ -0,0 +1,56 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "missing_srps_lambda_role" {
+  name               = "missing_srps_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "missing_srps_lambda_basic_policy" {
+  role       = aws_iam_role.missing_srps_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
+resource "aws_iam_role_policy" "missing_srps_sqs_policy" {
+  name = "missing_srps_lambda_sqs"
+  role = aws_iam_role.missing_srps_lambda_role.name
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+        Effect   = "Allow"
+        Resource = var.pending_srp_sqs_arn
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "user_query_sqs_policy" {
+  name = "user_query_lambda_sqs"
+  role = aws_iam_role.missing_srps_lambda_role.name
+  policy = jsonencode({
+    Statement = [
+      {
+        Action   = ["sqs:sendmessage"]
+        Effect   = "Allow"
+        Resource = [var.study_summaries_sqs_arn]
+      },
+    ]
+  })
+}
--- infra/lambdas/study_summaries/cloudwatch.tf
+++ infra/lambdas/study_summaries/cloudwatch.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "user_query_logs" {
+resource "aws_cloudwatch_log_group" "study_summaries_logs" {
   name              = "/aws/lambda/${aws_lambda_function.study_summaries.function_name}"
   retention_in_days = 30
 }
