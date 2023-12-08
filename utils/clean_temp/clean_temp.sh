0270bbe004443d5f49a43f3e721a8c5dc8338fe7
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-12-08 12:37:45 +0100

    :recycle: Clean assets

--- utils/clean_temp/clean_temp.sh
+++ utils/clean_temp/clean_temp.sh
@@ -2,5 +2,8 @@
 
 rm -rf infra/.terraform
 find . -name "*.zip" -type f -delete
+find . -name "bundle" -type d -exec rm -r "{}" \;
+find infra -name "env_*" -type d -exec rm -r "{}" \;
+find . -name ".terraform.lock.hcl" -type f -delete
 
 echo "Temp files and folders have been deleted"
