0f844340f827bed20311b646269b8f4c689ae1f7
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-12-08 18:38:53 +0100

    Add DLQs

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -2,3 +2,42 @@ resource "aws_cloudwatch_log_group" "sra_collector_logs" {
   name              = "/aws/api_gateway/${aws_apigatewayv2_api.sra_collector_api.name}"
   retention_in_days = 30
 }
+
+resource "aws_cloudwatch_metric_alarm" "study_ids_dlq_overfill_alarm" {
+  alarm_name          = "study_ids_dlq_overfill"
+  evaluation_periods  = 1
+  period              = 300
+  namespace           = "AWS/SQS"
+  dimensions          = { QueueName = aws_sqs_queue.study_ids_dlq.name }
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = 1
+  statistic           = "Sum"
+  treat_missing_data  = "ignore"
+}
+
+resource "aws_cloudwatch_metric_alarm" "pending_srp_dlq-overfill-alarm" {
+  alarm_name          = "pending_srp_dlq_overfill"
+  evaluation_periods  = 1
+  period              = 300
+  namespace           = "AWS/SQS"
+  dimensions          = { QueueName = aws_sqs_queue.pending_srp_dlq.name }
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = 1
+  statistic           = "Sum"
+  treat_missing_data  = "ignore"
+}
+
+resource "aws_cloudwatch_metric_alarm" "study_summaries_dlq_overfill_alarm" {
+  alarm_name          = "study_summaries_dlq_overfill"
+  evaluation_periods  = 1
+  period              = 300
+  namespace           = "AWS/SQS"
+  dimensions          = { QueueName = aws_sqs_queue.study_ids_dlq.name }
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold           = 1
+  statistic           = "Sum"
+  treat_missing_data  = "ignore"
+}
--- infra/sqs.tf
+++ infra/sqs.tf
@@ -1,11 +1,35 @@
 resource "aws_sqs_queue" "study_ids_queue" {
   name = "study_ids_queue"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.study_ids_dlq.arn,
+    maxReceiveCount     = 2
+  })
+}
+
+resource "aws_sqs_queue" "study_ids_dlq" {
+  name = "study_ids_dlq"
 }
 
 resource "aws_sqs_queue" "pending_srp_queue" {
   name = "pending_srp_queue"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.pending_srp_dlq.arn,
+    maxReceiveCount     = 2
+  })
+}
+
+resource "aws_sqs_queue" "pending_srp_dlq" {
+  name = "pending_srp_dlq"
 }
 
 resource "aws_sqs_queue" "study_summaries_queue" {
   name = "study_summaries_queue"
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.study_summaries_dlq.arn,
+    maxReceiveCount     = 2
+  })
+}
+
+resource "aws_sqs_queue" "study_summaries_dlq" {
+  name = "study_summaries_dlq"
 }
