ec12efedf76aacb13ae259f2a39ac40be1743f4d
arcones <marta.arcones@alumnos.upm.es>
2023-12-17 18:11:36 +0100

    📚 Create database (#17)

---
+++ infra/network.tf
@@ -0,0 +1,37 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.4.0"
+
+  name                 = "sra_collector_vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "db_subnet"
+  subnet_ids = module.vpc.public_subnets
+}
+
+resource "aws_security_group" "db_security_group" {
+  name   = "db_security_group"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ infra/rds.tf
@@ -0,0 +1,19 @@
+resource "aws_kms_key" "db_kms_key" {
+  description = "KMS for SRA collector database"
+}
+
+resource "aws_db_instance" "sra_collector_db" {
+  allocated_storage             = 5
+  db_name                       = "sracollector"
+  engine                        = "postgres"
+  engine_version                = "16.1"
+  instance_class                = "db.t4g.micro"
+  identifier                    = "sracollector"
+  manage_master_user_password   = true
+  master_user_secret_kms_key_id = aws_kms_key.db_kms_key.key_id
+  username                      = "sracollector"
+  db_subnet_group_name          = aws_db_subnet_group.db_subnet.name
+  vpc_security_group_ids        = [aws_security_group.db_security_group.id]
+  publicly_accessible           = true
+  skip_final_snapshot           = true
+}
