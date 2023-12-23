7e5185b497e398d5eeb7e6a9b081594bac182efb
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2023-12-23 17:25:00 +0100

    WIP

--- infra/lambdas/1_paginate_user_query/iam.tf
+++ infra/lambdas/1_paginate_user_query/iam.tf
@@ -67,3 +67,33 @@ resource "aws_iam_role_policy" "ssm_policy" {
     ]
   })
 }
+
+resource "aws_iam_role_policy" "secret_policy" {
+  name = "secret_policy"
+  role = aws_iam_role.lambda_assume.name
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Action   = ["secretsmanager:GetSecretValue"]
+        Effect   = "Allow"
+        Resource = "arn:aws:secretsmanager:eu-central-1:120715685161:secret:rds!db-3ce19e76-772e-4b32-b2b1-fc3e6d54c7f6-bNjnDK"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "kms_policy" {
+  name = "kms_policy"
+  role = aws_iam_role.lambda_assume.name
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Action   = ["kms:*"]
+        Effect   = "Allow"
+        Resource = "arn:aws:kms:eu-central-1:120715685161:key/33d5669b-6a9e-4055-be8c-8231920dd860"
+      },
+    ]
+  })
+}
--- infra/lambdas/common_libs_layer.tf
+++ infra/lambdas/common_libs_layer.tf
@@ -6,6 +6,7 @@ resource "aws_s3_object" "common_libs_lambda_layer_zip" {
   bucket = var.s3_bucket_id
   source = "${path.module}/${local.lib_name}.zip"
   key    = "${local.lib_name}.zip"
+  etag   = filemd5("${path.module}/${local.lib_name}.zip")
 }
 
 resource "aws_lambda_layer_version" "common_libs_lambda_layer" {
