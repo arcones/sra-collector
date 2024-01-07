b07bb1ea51668e413d0da866a00d4cd74a11aa72
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2024-01-07 17:19:05 +0100

    WIP

--- infra/opensearch/lambda/cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "cloudwatch_to_opensearch_logs" {
-  name              = "/aws/lambda/${aws_lambda_function.cloudwatch_to_opensearch.function_name}"
-  retention_in_days = 7
-}
