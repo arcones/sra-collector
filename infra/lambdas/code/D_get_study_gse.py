ed8a961937222d09935ac809e5be5bed4d307fa5
Marta Arcones Rodríguez <marta.arconesrodriguez@zooplus.com>
2024-02-06 16:12:33 +0100

    polish D

--- infra/lambdas/code/D_get_study_gse.py
+++ infra/lambdas/code/D_get_study_gse.py
@@ -18,31 +18,31 @@ def handler(event, context):
         if event:
             logging.info(f'Received event {event}')
 
-        ncbi_api_key_secret = secrets.get_secret_value(SecretId='ncbi_api_key_secret')
-        ncbi_api_key = json.loads(ncbi_api_key_secret['SecretString'])['value']
+            ncbi_api_key_secret = secrets.get_secret_value(SecretId='ncbi_api_key_secret')
+            ncbi_api_key = json.loads(ncbi_api_key_secret['SecretString'])['value']
 
-        base_url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gds&retmode=json&api_key={ncbi_api_key}'
+            base_url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gds&retmode=json&api_key={ncbi_api_key}'
 
-        for record in event['Records']:
-            study_request = json.loads(record['body'])
-            study_id = str(study_request['study_id'])
-            request_info = study_request['request_info']
+            for record in event['Records']:
+                study_request = json.loads(record['body'])
+                study_id = str(study_request['study_id'])
+                request_info = study_request['request_info']
 
-            url = f'{base_url}&id={study_id}'
-            logging.debug(f'The URL is {url}')
-            response_status = 0
+                url = f'{base_url}&id={study_id}'
+                logging.debug(f'The URL is {url}')
+                response_status = 0
 
-            while response_status != 200:
-                response = http.request('GET', url)
-                response_status = response.status
-                if response_status == 200:
-                    logging.debug(f'The response is {response.data}')
-                    summary = json.loads(response.data)['result'][study_id]
-                    _summary_process(context.function_name, study_id, request_info, summary)
-                else:
-                    logging.warning(f'API Limit reached, retrying')
-                    time.sleep(1)
-                    continue
+                while response_status != 200:
+                    response = http.request('GET', url)
+                    response_status = response.status
+                    if response_status == 200:
+                        logging.debug(f'The response is {response.data}')
+                        summary = json.loads(response.data)['result'][study_id]
+                        _summary_process(context.function_name, study_id, request_info, summary)
+                    else:
+                        logging.warning(f'API Limit reached, retrying')
+                        time.sleep(1)
+                        continue
 
                 return {'statusCode': 200}
     except Exception as e:
@@ -52,9 +52,7 @@ def handler(event, context):
 
 def _summary_process(function_name: str, study_id: str, request_info: dict, summary: str):
     output_sqs, schema = env_params.params_per_env(function_name)
-
     logging.debug(f'Study summary from study {study_id} is {summary}')
-
     gse = _extract_gse_from_summaries(summary)
 
     if gse:
